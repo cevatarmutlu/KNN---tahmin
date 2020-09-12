12246948d98057d1de1f4dac56b8a88df0aaa59c
cevat <cevatarmutlu@gmail.com>
2020-09-12 12:49:37 +0300

    update

---
+++ README.md
@@ -0,0 +1,21 @@
+# KNN TAHMİN
+iris veriseti kullanılarak yapılmıştır.
+Verisetindeki etiketlerin silinmesi gerekmektedir. Sadece hücrelerdeki eksik verileri tahmin etmektedir.
+
+# KURULUM
+```
+pip3 install -r requirenments
+```
+
+# KUllanım
+
+- Elinizdeki verisetinin etiketlerini temizleyin
+- Tahmin etmesini istediğiniz hücrenin satırını ```knn_sorgu.xlsx``` adlı excel dosyasının içine yapıştırın.
+- Çıktı olarak ```knn_tahmin.xlsx``` adlı bir çıktı vermektedir
+
+## Eksikleri
+
+- Satırların son hücrelerindeki veriler eksik olursa tahmin etmiyor
+- Kod yazımı karışık
+
+
--- tahmin_knn.py
+++ tahmin_knn.py
@@ -2,8 +2,6 @@ import xlrd
 import math
 from heapq import nsmallest
 from xlwt import Workbook
-wb = Workbook()
-sheet = wb.add_sheet("tahmin")
 
 def print_rows(excel):
     for row in range(excel.nrows):
@@ -20,6 +18,12 @@ def knn_sorgu_excel(excel_path, excel_sheet_index: int):
     return sheet
 
 
+def tahmin_deger_getir(array):
+    top = 0
+    for i in range(len(array)):
+        top += float(knn.excel.row_values(array[i])[sorgu_index]) / len(array)
+    return top
+
 class KNN:
     def __init__(self, num_k: int, excel_path, excel_sheet_index: int):
         self.k = num_k
@@ -32,37 +36,32 @@ class KNN:
         sheet = wb.sheet_by_index(self.sheet_index)
         return sheet
 
+if __name__ == "__main__":
+    wb = Workbook()
+    sheet = wb.add_sheet("tahmin")
 
-knn_sorgu = knn_sorgu_excel("../knn_sorgu.xlsx", 0)
-knn = KNN(3, "../bezdekIris.xlsx", 0)
-for a in range(knn_sorgu.nrows):
-    sorgu = knn_sorgu.row_values(a)
-    sorgu_index = sorgu.index('')
-    sorgu.pop(sorgu_index)
-    uzaklik_array = []
-    knn_uzaklik = []
-    tahmin_deger = 0
+    knn_sorgu = knn_sorgu_excel("knn_sorgu.xlsx", 0)
+    knn = KNN(3, "bezdekIris.xlsx", 0)
+    for a in range(knn_sorgu.nrows):
+        sorgu = knn_sorgu.row_values(a)
+        sorgu_index = sorgu.index('')
+        sorgu.pop(sorgu_index)
+        uzaklik_array = []
+        knn_uzaklik = []
+        tahmin_deger = 0
 
-    for i in range(knn.excel.nrows):
-        kayit = list(map(float, knn.excel.row_values(i)))
-        kayit.pop(sorgu_index)
-        sorgu = list(map(float, sorgu))
-        uzaklik_array.append(euclidean(sorgu, kayit))
+        for i in range(knn.excel.nrows):
+            kayit = list(map(float, knn.excel.row_values(i)))
+            kayit.pop(sorgu_index)
+            sorgu = list(map(float, sorgu))
+            uzaklik_array.append(euclidean(sorgu, kayit))
 
-    en_yakin_sorgu_index = nsmallest(knn.k, uzaklik_array)
-    for i in range(len(en_yakin_sorgu_index)):
-        knn_uzaklik.append(uzaklik_array.index(en_yakin_sorgu_index[i]))
+        en_yakin_sorgu_index = nsmallest(knn.k, uzaklik_array)
+        for i in range(len(en_yakin_sorgu_index)):
+            knn_uzaklik.append(uzaklik_array.index(en_yakin_sorgu_index[i]))
 
+        tahmin_deger = tahmin_deger_getir(knn_uzaklik)
+        sheet.write(a, 0, tahmin_deger)
+        print(tahmin_deger)
 
-    def tahmin_deger_getir(array):
-        top = 0
-        for i in range(len(array)):
-            top += float(knn.excel.row_values(array[i])[sorgu_index]) / len(array)
-        return top
-
-
-    tahmin_deger = tahmin_deger_getir(knn_uzaklik)
-    sheet.write(a, 0, tahmin_deger)
-    print(tahmin_deger)
-
-wb.save("knn_tahmin.xlsx")
+    wb.save("knn_tahmin.xlsx")
